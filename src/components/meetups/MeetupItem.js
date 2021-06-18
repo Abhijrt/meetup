b8a847bbac8ea92b10fcc8272c51e0f9e3286dc2
Abhay Jirati <abhayjirati@Abhays-MacBook-Air.local>
2021-06-18 09:26:06 +0530

    showing the meetup on the page

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,16 +1,17 @@
 import classes from './MeetupItem.module.css';
+import Card from '../ui/Card';
 
-function MeetupItem () {
+function MeetupItem (props) {
     return (
         <li className={classes.item}>
             <Card>
                 <div className={classes.image}>
-                <img src={} alt={} />
+                <img src={props.image} alt={props.title} />
                 </div>
                 <div className={classes.content}>
-                <h3></h3>
-                <address></address>
-                <p></p>
+                <h3>{props.title}</h3>
+                <address>{props.address}</address>
+                <p>{props.description}</p>
                 </div>
                 <div className={classes.actions}>
                 <button>
--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -1,6 +1,28 @@
-function AllMeetupsPage () {
+import { useEffect, useState } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
 
-    return <div>All Meetup</div>
+function AllMeetupsPage () {
+    const [meetUpData, setMeetUpData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://react-summery-default-rtdb.firebaseio.com/meetup.json')
+        .then((response) => response.json())
+        .then((data) => {
+            const meetUpData = [];
+            for(const key in data) {
+                const meetup = {
+                    id: key,
+                    ...data[key]
+                }
+                meetUpData.push(meetup);
+            }
+            setMeetUpData(meetUpData);
+        });
+    },[])
+
+    return <section>
+        <MeetupList meetups={meetUpData} />
+    </section>
 }
 
 export default AllMeetupsPage;
