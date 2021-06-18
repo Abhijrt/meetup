822d1e89ab464b34fdde7db9460e7b61c4c994c5
Abhay Jirati <abhayjirati@Abhays-MacBook-Air.local>
2021-06-18 22:00:50 +0530

    adding the Favourites data

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,5 +1,23 @@
+import { useContext } from 'react';
+import FavouriteContext from '../store/favourites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 function Favourites () {
-    return <div>Favoutires</div>
+
+    const FavoutireCtx = useContext(FavouriteContext);
+
+    let content ;
+
+    if(FavoutireCtx.totalFavouritesCount === 0) {
+        content = <p>You Don't Have any Favourite Meetup</p>
+    }else{
+        content = <MeetupList meetups={FavoutireCtx.favourites}/>
+    }
+
+    return <section>
+        <h1>My favourite</h1>
+        {content}
+    </section>
 }
 
 export default Favourites;
